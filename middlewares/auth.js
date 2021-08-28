79334ab5a9c8a92f2e75202a388200a7e7b30482
Natesh <natesh246@gmail.com>
2021-08-28 15:47:04 +0530

    verify Token responds with 401

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,16 +7,16 @@ function verifyToken(req, res, next) {
       return res.status(401).json({
         data: null,
         status: 401,
-        message: "Token is required for authentication",
+        message: "not authenticated",
       });
     const decodedToken = jwt.verify(token, "secret");
     req.user = decodedToken;
     return next();
   } catch (error) {
-    return res.status(500).json({
+    return res.status(401).json({
       data: null,
-      status: 500,
-      message: "Invalid token",
+      status: 401,
+      message: "not authenticated",
     });
   }
 }
