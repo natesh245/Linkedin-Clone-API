6423e78997363cab8ff392e507e5b58ada2ec9b7
Natesh <natesh246@gmail.com>
2022-01-22 15:00:04 +0530

    400 status code on missing token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,10 @@ function verifyToken(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token)
-      return res.status(401).json({
+      return res.status(400).json({
         data: null,
-        status: 401,
-        message: "not authenticated",
+        status: 400,
+        message: "Bad Request- missing token",
       });
     const decodedToken = jwt.verify(token, "secret");
     req.user = decodedToken;
